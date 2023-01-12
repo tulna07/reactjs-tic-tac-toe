fb11fa2b837c7679616ca89e7ffaa305f85b9094
scoobytux <tu.lna07@gmail.com>
2023-01-12 13:27:36 +0700

    feat: determine game status

--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -10,8 +10,20 @@ const Board = ({ size }) => {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(size ** 2).fill(null));
 
+  let status = `Next turn: ${xIsNext ? "X" : "O"}`;
+
+  const isDraw = checkDraw(squares);
+  if (isDraw) {
+    status = `No one win!!!`;
+  }
+
+  const winner = findWinner(squares);
+  if (winner) {
+    status = `Winner: ${winner}`;
+  }
+
   const handleClick = itemId => {
-    if (squares[itemId]) {
+    if (squares[itemId] || winner) {
       return;
     }
 
@@ -44,7 +56,41 @@ const Board = ({ size }) => {
     return board;
   };
 
-  return <div className={styles.board}>{renderBoard()}</div>;
+  return (
+    <>
+      <div className="status">{status}</div>
+      <div className={styles.board}>{renderBoard()}</div>
+    </>
+  );
 };
+
+function checkDraw(squares) {
+  for (const square of squares) {
+    if (!square) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function findWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 
 export default Board;
